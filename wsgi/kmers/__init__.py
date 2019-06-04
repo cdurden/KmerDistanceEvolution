2ae10f74a5ce8287c392845ea62e8efe5c1ec8b2
Chris Durden <cdurden@gmail.com>
2019-06-03 22:25:29 -0400

    renaming reconstuction to tree_estimate

--- wsgi/kmers/__init__.py
+++ wsgi/kmers/__init__.py
@@ -54,20 +54,20 @@ def huelsenbeck_svg(request):
     import tempfile
     plt.ioff()
     cmap = cm.jet
-    reconstruction_set_id = request.matchdict['reconstruction_set_id']
+    tree_estimate_set_id = request.matchdict['tree_estimate_set_id']
 #    method=request.matchdict['method']
 #    m=request.matchdict['m']
 #    genes=request.matchdict['genes']
 #    kvd_method=request.matchdict['kvd_method']
 #    k=tuple(request.matchdict['k'].split("_"))
 
-    reconstruction_set = DBSession.query(HuelsenbeckTreeEstimateSet).get(reconstruction_set_id)
+    tree_estimate_set = DBSession.query(HuelsenbeckTreeEstimateSet).get(tree_estimate_set_id)
     from sqlalchemy.orm import joinedload
     from sqlalchemy.orm import defaultload
-    a_max = reconstruction_set.simulation_set.a_max
-    b_max = reconstruction_set.simulation_set.b_max
-    nr_rows = reconstruction_set.simulation_set.rows
-    nr_cols = reconstruction_set.simulation_set.cols
+    a_max = tree_estimate_set.simulation_set.a_max
+    b_max = tree_estimate_set.simulation_set.b_max
+    nr_rows = tree_estimate_set.simulation_set.rows
+    nr_cols = tree_estimate_set.simulation_set.cols
 
     # set of arrays to store cell data
     n = np.zeros((nr_rows, nr_cols))
@@ -77,20 +77,20 @@ def huelsenbeck_svg(request):
 #        print(row)
 #        for col in range(nr_cols):
 #            print(col)
-#            huelsenbeck_reconstructions = DBSession.query(HuelsenbeckTreeEstimate).filter(reconstruction_set==reconstruction_set).filter(row==row).filter(col==col).all()
-#            n[row,col] = sum([huelsenbeck_reconstruction.success for huelsenbeck_reconstruction in huelsenbeck_reconstructions])
-#            d[row,col] = len(huelsenbeck_reconstructions)
+#            huelsenbeck_tree_estimates = DBSession.query(HuelsenbeckTreeEstimate).filter(tree_estimate_set==tree_estimate_set).filter(row==row).filter(col==col).all()
+#            n[row,col] = sum([huelsenbeck_tree_estimate.success for huelsenbeck_tree_estimate in huelsenbeck_tree_estimates])
+#            d[row,col] = len(huelsenbeck_tree_estimates)
 #
 
     # Name the output file
 
 
     # Read input data from FITS file
-    for huelsenbeck_reconstruction in reconstruction_set.huelsenbeck_reconstructions:
-        row = huelsenbeck_reconstruction.huelsenbeck_simulation.row
-        col = huelsenbeck_reconstruction.huelsenbeck_simulation.col
+    for huelsenbeck_tree_estimate in tree_estimate_set.huelsenbeck_tree_estimates:
+        row = huelsenbeck_tree_estimate.huelsenbeck_simulation.row
+        col = huelsenbeck_tree_estimate.huelsenbeck_simulation.col
         d[row,col] += 1
-        if bool(huelsenbeck_reconstruction.reconstruction.success):
+        if bool(huelsenbeck_tree_estimate.tree_estimate.success):
             n[row,col] += 1
     print(n)
     print(d)
@@ -152,43 +152,43 @@ def huelsenbeck_svg(request):
 
 def results(request):
     sim_parameters = ['n','indelible_model','genes','m','theta']
-    reconstruction_parameters = ['k','distance_formula','alignment_method','method']
+    tree_estimate_parameters = ['k','distance_formula','alignment_method','method']
     simulation_sets = DBSession.query(HuelsenbeckSimulationSet).options(Load(HuelsenbeckSimulationSet).lazyload('*')).all()
-    reconstruction_sets = DBSession.query(HuelsenbeckTreeEstimateSet).options(Load(HuelsenbeckTreeEstimateSet).lazyload('*')).all()
+    tree_estimate_sets = DBSession.query(HuelsenbeckTreeEstimateSet).options(Load(HuelsenbeckTreeEstimateSet).lazyload('*')).all()
     sim_parameters_dict = dict([(parameter,set([getattr(simulation_set,parameter) for simulation_set in simulation_sets])) for parameter in sim_parameters])
-    reconstruction_parameters_dict = dict([(parameter,set([getattr(reconstruction_set,parameter) for reconstruction_set in reconstruction_sets])) for parameter in reconstruction_parameters])
+    tree_estimate_parameters_dict = dict([(parameter,set([getattr(tree_estimate_set,parameter) for tree_estimate_set in tree_estimate_sets])) for parameter in tree_estimate_parameters])
     import itertools
     sim_parameter_combos = list(itertools.product(*[sim_parameters_dict[sim_parameter] for sim_parameter in sim_parameters]))
-    reconstruction_parameter_combos = list(itertools.product(*[reconstruction_parameters_dict[reconstruction_parameter] for reconstruction_parameter in reconstruction_parameters]))
+    tree_estimate_parameter_combos = list(itertools.product(*[tree_estimate_parameters_dict[tree_estimate_parameter] for tree_estimate_parameter in tree_estimate_parameters]))
     sim_parameter_combos.sort(key=lambda x: x if isinstance(x, str) else str(x))
-    reconstruction_parameter_combos.sort(key=lambda x: x if isinstance(x, str) else str(x))
-    results = np.empty((len(sim_parameter_combos),len(reconstruction_parameter_combos)),dtype=tuple)
+    tree_estimate_parameter_combos.sort(key=lambda x: x if isinstance(x, str) else str(x))
+    results = np.empty((len(sim_parameter_combos),len(tree_estimate_parameter_combos)),dtype=tuple)
     nonempty_cols = list()
     for row,sim_parameter_combo in enumerate(sim_parameter_combos):
-        for col,reconstruction_parameter_combo in enumerate(reconstruction_parameter_combos):
+        for col,tree_estimate_parameter_combo in enumerate(tree_estimate_parameter_combos):
             q = DBSession.query(HuelsenbeckTreeEstimateSet).\
                     join(HuelsenbeckTreeEstimateSet.simulation_set, aliased=True).\
-                    options(lazyload(HuelsenbeckTreeEstimateSet.huelsenbeck_reconstructions))
+                    options(lazyload(HuelsenbeckTreeEstimateSet.huelsenbeck_tree_estimates))
             for param, value in zip(sim_parameters,sim_parameter_combo):
                     q = q.filter(getattr(HuelsenbeckSimulationSet, param)==value)
-            for param, value in zip(reconstruction_parameters,reconstruction_parameter_combo):
+            for param, value in zip(tree_estimate_parameters,tree_estimate_parameter_combo):
                     q = q.filter(getattr(HuelsenbeckTreeEstimateSet, param)==value)
-            results[row,col]= tuple([reconstruction_set.id for reconstruction_set in q.all()])
+            results[row,col]= tuple([tree_estimate_set.id for tree_estimate_set in q.all()])
             if len(results[row,col])>0:
                 nonempty_cols.append(col)
     nonempty_cols = list(set(nonempty_cols))
     results = results[:,nonempty_cols]
-    reconstruction_parameter_combos = [reconstruction_parameter_combos[col] for col in nonempty_cols]
-    return({'results': results, 'sim_parameters':sim_parameters, 'reconstruction_parameters': reconstruction_parameters, 'sim_parameter_combos':sim_parameter_combos, 'reconstruction_parameter_combos':reconstruction_parameter_combos})
+    tree_estimate_parameter_combos = [tree_estimate_parameter_combos[col] for col in nonempty_cols]
+    return({'results': results, 'sim_parameters':sim_parameters, 'tree_estimate_parameters': tree_estimate_parameters, 'sim_parameter_combos':sim_parameter_combos, 'tree_estimate_parameter_combos':tree_estimate_parameter_combos})
 
 
-def huelsenbeck_reconstruction_set(request):
+def huelsenbeck_tree_estimate_set(request):
     import numpy as np
 
-    reconstruction_set_id = request.matchdict['reconstruction_set_id']
+    tree_estimate_set_id = request.matchdict['tree_estimate_set_id']
 
-    reconstruction_set = DBSession.query(HuelsenbeckTreeEstimateSet).get(reconstruction_set_id)
-    return({'huelsenbeck_reconstruction_set': reconstruction_set, 'np': np})
+    tree_estimate_set = DBSession.query(HuelsenbeckTreeEstimateSet).get(tree_estimate_set_id)
+    return({'huelsenbeck_tree_estimate_set': tree_estimate_set, 'np': np})
 
 def huelsenbeck(request):
     try:
@@ -226,13 +226,13 @@ def huelsenbeck(request):
                                                    (HuelsenbeckSimulationSet.theta, theta),
                                                    (HuelsenbeckSimulationSet.m, m)] if len(vals)>0]
 
-    reconstruction_set_macro = get_renderer("templates/huelsenbeck_reconstruction_set.pt").implementation().macros
-    huelsenbeck_reconstruction_sets = DBSession.query(HuelsenbeckTreeEstimateSet).\
+    tree_estimate_set_macro = get_renderer("templates/huelsenbeck_tree_estimate_set.pt").implementation().macros
+    huelsenbeck_tree_estimate_sets = DBSession.query(HuelsenbeckTreeEstimateSet).\
             join(HuelsenbeckTreeEstimateSet.simulation_set, aliased=True).\
             options(subqueryload(HuelsenbeckTreeEstimateSet.simulation_set)).\
             filter(and_(*filter_group)).all()
-    #huelsenbeck_reconstruction_sets.sort(key=lambda huelsenbeck_reconstruction_set: sum(huelsenbeck_reconstruction.reconstruction.success for huelsenbeck_reconstruction in huelsenbeck_reconstruction_set.huelsenbeck_reconstructions),reverse=True)
-    return({'methods': methods, 'distance_formulas': distance_formulas, 'k': ",".join(k), 'm': m, 'genes': genes, 'huelsenbeck_reconstruction_sets': huelsenbeck_reconstruction_sets, 'results': [], 'reconstruction_set_macro': reconstruction_set_macro, 'np': np})
+    #huelsenbeck_tree_estimate_sets.sort(key=lambda huelsenbeck_tree_estimate_set: sum(huelsenbeck_tree_estimate.tree_estimate.success for huelsenbeck_tree_estimate in huelsenbeck_tree_estimate_set.huelsenbeck_tree_estimates),reverse=True)
+    return({'methods': methods, 'distance_formulas': distance_formulas, 'k': ",".join(k), 'm': m, 'genes': genes, 'huelsenbeck_tree_estimate_sets': huelsenbeck_tree_estimate_sets, 'results': [], 'tree_estimate_set_macro': tree_estimate_set_macro, 'np': np})
 
 if __name__ == '__main__':
     import os
@@ -240,10 +240,10 @@ if __name__ == '__main__':
     config = Configurator()
     config.include('pyramid_chameleon')
     #config.add_route('huelsenbeck', '/huelsenbeck/{method}/{distance_formula}/{genes}/{m}/{k}')
-    config.add_route('huelsenbeck_svg', '/huelsenbeck_svg/{reconstruction_set_id}.svg')
-    config.add_view(huelsenbeck_svg, route_name='huelsenbeck_svg', renderer=os.path.join(here,'templates','huelsenbeck_reconstruction_set.pt'))
-    config.add_route('huelsenbeck_reconstruction_set', '/huelsenbeck_reconstruction_set/{reconstruction_set_id}')
-    config.add_view(huelsenbeck_reconstruction_set, route_name='huelsenbeck_reconstruction_set', renderer=os.path.join(here,'templates','huelsenbeck_reconstruction_set.pt'))
+    config.add_route('huelsenbeck_svg', '/huelsenbeck_svg/{tree_estimate_set_id}.svg')
+    config.add_view(huelsenbeck_svg, route_name='huelsenbeck_svg', renderer=os.path.join(here,'templates','huelsenbeck_tree_estimate_set.pt'))
+    config.add_route('huelsenbeck_tree_estimate_set', '/huelsenbeck_tree_estimate_set/{tree_estimate_set_id}')
+    config.add_view(huelsenbeck_tree_estimate_set, route_name='huelsenbeck_tree_estimate_set', renderer=os.path.join(here,'templates','huelsenbeck_tree_estimate_set.pt'))
     config.add_route('huelsenbeck', '/huelsenbeck')
     config.add_view(huelsenbeck, route_name='huelsenbeck', renderer=os.path.join(here,'templates','huelsenbeck.pt'))
     config.add_route('results', '/results')
@@ -266,16 +266,16 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
 
     here = os.path.dirname(os.path.abspath(__file__))
-    config.add_route('huelsenbeck_svg', '/huelsenbeck_svg/{reconstruction_set_id}.svg')
+    config.add_route('huelsenbeck_svg', '/huelsenbeck_svg/{tree_estimate_set_id}.svg')
     config.add_view(huelsenbeck_svg, route_name='huelsenbeck_svg')
-    #config.add_view(huelsenbeck_svg, route_name='huelsenbeck_svg', renderer=os.path.join(here,'templates','huelsenbeck_reconstruction_set.pt'))
-    config.add_route('huelsenbeck_reconstruction_set', '/huelsenbeck_reconstruction_set/{reconstruction_set_id}')
-    config.add_view(huelsenbeck_reconstruction_set, route_name='huelsenbeck_reconstruction_set', renderer=os.path.join(here,'templates','huelsenbeck_reconstruction_set.pt'))
+    #config.add_view(huelsenbeck_svg, route_name='huelsenbeck_svg', renderer=os.path.join(here,'templates','huelsenbeck_tree_estimate_set.pt'))
+    config.add_route('huelsenbeck_tree_estimate_set', '/huelsenbeck_tree_estimate_set/{tree_estimate_set_id}')
+    config.add_view(huelsenbeck_tree_estimate_set, route_name='huelsenbeck_tree_estimate_set', renderer=os.path.join(here,'templates','huelsenbeck_tree_estimate_set.pt'))
     config.add_route('huelsenbeck', '/huelsenbeck')
-    config.add_route('huelsenbeck_reconstruction_sets', '/huelsenbeck_reconstruction_sets/{method}/{distance_formula}/{k}')
+    config.add_route('huelsenbeck_tree_estimate_sets', '/huelsenbeck_tree_estimate_sets/{method}/{distance_formula}/{k}')
     config.add_route('huelsenbeck_simulation_sets', '/huelsenbeck_simulation_sets/{n}/{indelible_model}/{genes}/{m}/{theta}')
     config.add_view(huelsenbeck, route_name='huelsenbeck', renderer=os.path.join(here,'templates','huelsenbeck.pt'))
-    config.add_view(huelsenbeck, route_name='huelsenbeck_reconstruction_sets', renderer=os.path.join(here,'templates','huelsenbeck.pt'))
+    config.add_view(huelsenbeck, route_name='huelsenbeck_tree_estimate_sets', renderer=os.path.join(here,'templates','huelsenbeck.pt'))
     config.add_view(huelsenbeck, route_name='huelsenbeck_simulation_sets', renderer=os.path.join(here,'templates','huelsenbeck.pt'))
     config.add_route('results', '/results')
     config.add_view(results, route_name='results', renderer=os.path.join(here,'templates','results.pt'))
